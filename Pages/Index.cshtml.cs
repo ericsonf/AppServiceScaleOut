f8d166d38c68c4824ddd2a60951061b9cb6e8801
Ericson da Fonseca <ericsonf@outlook.com>
2020-02-10 22:56:41 -0300

    Initial Commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace AppServiceScaleOut.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string MachineName { get; set; }
+        public string OS { get; set; }
+        public int ProcessorCount { get; set ;}
+        public string Framework { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            MachineName = Environment.MachineName;
+            OS = Environment.OSVersion.VersionString;
+            ProcessorCount = Environment.ProcessorCount;
+            Framework = Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName;
+        }
+    }
+}
